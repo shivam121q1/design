cc8b38d8c146f72f26c9cb9a320c78ed750c864b
shivam121q1 <shivampal7006@gmail.com>
2025-02-01 16:07:38 +0530

    Feature bar updation

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -2,26 +2,28 @@ import { Database, Wifi, MessageCircle, Smartphone } from "lucide-react";
 
 export default function FeaturesBar() {
   return (
-    <div className="w-[951px] flex justify-between items-center py-2  mx-auto">
-      <div className="flex gap-4 items-center px-4">
-        <Database className="w-6 h-6 mb-2 text-black text-primary" />
-        <p className="text-sm font-medium ">Unlimited Talk & Text</p>
-        <div className="bg-black bg-primary border-2 ml-2 w-1 h-10 "></div>
+    <div className="w-full max-w-[951px] flex flex-wrap justify-center md:justify-between items-center py-4 mx-auto gap-6 md:gap-4">
+      {/** Feature Item */}
+      <div className="flex gap-3 items-center px-4">
+        <Database className="w-6 h-6 text-primary" />
+        <p className="text-sm font-medium">Unlimited Talk & Text</p>
+        <div className="hidden md:block bg-primary w-1 h-10"></div>
       </div>
-      <div className="flex gap-4 items-center px-4">
-        <Wifi className="w-6 h-6 mb-2 text-black text-primary" />
+
+      <div className="flex gap-3 items-center px-4">
+        <Wifi className="w-6 h-6 text-primary" />
         <p className="text-sm font-medium">Nationwide Coverage</p>
-        <div className="bg-black bg-primary border-2 ml-2 w-1 h-10 "></div>
-
+        <div className="hidden md:block bg-primary w-1 h-10"></div>
       </div>
-      <div className="flex gap-4 items-center px-4 h-10">
-        <MessageCircle className="w-6 h-6 mb-2 text-black text-primary" />
+
+      <div className="flex gap-3 items-center px-4">
+        <MessageCircle className="w-6 h-6 text-primary" />
         <p className="text-sm font-medium">24/7 Customer Support</p>
-        <div className="bg-black bg-primary border-2 ml-2 w-1 h-10 "></div>
-
+        <div className="hidden md:block bg-primary w-1 h-10"></div>
       </div>
-      <div className="flex gap-4 items-center px-4">
-        <Smartphone className="w-6 h-6 mb-2 text-black text-primary" />
+
+      <div className="flex gap-3 items-center px-4">
+        <Smartphone className="w-6 h-6 text-primary" />
         <p className="text-sm font-medium">Hotspot Available</p>
       </div>
     </div>
